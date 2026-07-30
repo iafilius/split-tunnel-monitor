1a0a17090687af84c7cdc490961e3cae9832ca6f
Arjan Filius <Arjan.Filius@nn-group.com>
2026-07-30 20:02:26 +0200

    release: update brew formula to v1.0.1

--- Formula/split-tunnel-monitor.rb
+++ Formula/split-tunnel-monitor.rb
@@ -1,8 +1,8 @@
 class SplitTunnelMonitor < Formula
   desc "Split-tunnel VPN multipath monitor for macOS (Zscaler, AnyConnect, GlobalProtect)"
   homepage "https://github.com/iafilius/split-tunnel-monitor"
-  url "https://github.com/iafilius/split-tunnel-monitor/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d44f3424dfbcc5feba9d3393d4637a772432c604f168331ab7a1a949022e4313"
+  url "https://github.com/iafilius/split-tunnel-monitor/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "c9904d83d0f1272636a811acb6cfaf6557f01bddf830ec03511b2b2f6c4d6f9a"
   license "GPL-3.0-or-later"
 
   depends_on :macos
@@ -13,6 +13,6 @@ class SplitTunnelMonitor < Formula
   end
 
   test do
-    assert_match "split-tunnel-monitor 1.0.0", shell_output("#{bin}/split-tunnel-monitor --version")
+    assert_match "split-tunnel-monitor 1.0.1", shell_output("#{bin}/split-tunnel-monitor --version")
   end
 end
