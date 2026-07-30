8807db1117a23ee4ea1e67648465e6c426067a63
Arjan Filius <Arjan.Filius@nn-group.com>
2026-07-30 19:05:26 +0200

    release: update formula sha256 for v1.0.0 tarball

--- Formula/split-tunnel-monitor.rb
+++ Formula/split-tunnel-monitor.rb
@@ -2,7 +2,7 @@ class SplitTunnelMonitor < Formula
   desc "Split-tunnel VPN multipath monitor for macOS (Zscaler, AnyConnect, GlobalProtect)"
   homepage "https://github.com/iafilius/split-tunnel-monitor"
   url "https://github.com/iafilius/split-tunnel-monitor/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_REPLACE_AFTER_RELEASE"
+  sha256 "d44f3424dfbcc5feba9d3393d4637a772432c604f168331ab7a1a949022e4313"
   license "GPL-3.0-or-later"
 
   depends_on :macos
